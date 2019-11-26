a559439c00558781ba0495f7b097361a1aea1e97
cpfaff <ctpfaff@arcor.de>
2019-11-26 22:56:26 +0100

    Minor style change

--- app/models/dataset_paperproposal.rb
+++ app/models/dataset_paperproposal.rb
@@ -11,5 +11,5 @@ class DatasetPaperproposal < ActiveRecord::Base
   belongs_to :paperproposal
   belongs_to :dataset
 
-  validates :aspect, presence: true, inclusion: { in: %w(main side) }
+  validates :aspect, presence: true, inclusion: { in: %w[main side] }
 end
