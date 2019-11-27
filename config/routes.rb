dbf3c02e216f7932867d0136e6e813afeb8d6873
cpfaff <ctpfaff@arcor.de>
2019-11-27 08:27:49 +0100

    Updates configuration
    * Getting compatible with rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,29 @@
-Befchina::Application.routes.draw do
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
   root to: 'pages#home'
 
   resource :user_session, only: [:create]
-  match 'logout' => 'user_sessions#destroy', :as => :logout
+  match 'logout' => 'user_sessions#destroy', :as => :logout, via: [:get, :post]
 
   resources :users
-  resource :profile, only: [:show, :edit, :update] do
+  resource :profile, only: %i[show edit update] do
     member do
       get :votes, :votes_history
       post :update_credentials
-      resources :notifications, only: [:index, :destroy] do
+      resources :notifications, only: %i[index destroy] do
         get :mark_as_read, on: :member
       end
     end
   end
 
-  match 'imprint' => 'pages#imprint', :as => :imprint
+  match 'imprint' => 'pages#imprint', :as => :imprint, via: [:get, :post]
   get 'help' => redirect('https://github.com/befdata/befdata/wiki'), :as => :help
-  match 'data' => 'pages#data', :as => :data
-  match 'search' => 'pages#search'
+  match 'data' => 'pages#data', :as => :data, via: [:get, :post]
+  match 'search' => 'pages#search', via: [:get, :post]
 
   # resources :datasets, :except => [:index] do
   resources :datasets do
@@ -37,12 +42,12 @@ Befchina::Application.routes.draw do
   end
 
   scope path: '/files' do
-    resources :freeformats, only: [:create, :update, :destroy] do
+    resources :freeformats, only: %i[create update destroy] do
       get :download, on: :member
     end
   end
 
-  resources :keywords, controller: 'tags', only: [:index, :show] do
+  resources :keywords, controller: 'tags', only: %i[index show] do
     collection do
       get :manage
       post :delete, :pre_rename, :pre_merge, :rename, :merge
@@ -67,22 +72,22 @@ Befchina::Application.routes.draw do
       post :update_datasets
     end
   end
-  match 'paperproposals/update_vote/:id' => 'paperproposals#update_vote', :as => :update_vote
-  match 'paperproposals/update_state/:id' => 'paperproposals#update_state', :as => :paperproposal_update_state
+  match 'paperproposals/update_vote/:id' => 'paperproposals#update_vote', :as => :update_vote, via: [:get, :post, :patch]
+  match 'paperproposals/update_state/:id' => 'paperproposals#update_state', :as => :paperproposal_update_state, via: [:get, :post, :patch]
 
-  match 'create_cart_context/:dataset_id' => 'carts#create_cart_context', :as => :create_cart_context
-  match 'delete_cart_context/:dataset_id' => 'carts#delete_cart_context', :as => :delete_cart_context
-  match 'cart' => 'carts#show', :as => 'current_cart'
+  match 'create_cart_context/:dataset_id' => 'carts#create_cart_context', :as => :create_cart_context, via: [:get, :post]
+  match 'delete_cart_context/:dataset_id' => 'carts#delete_cart_context', :as => :delete_cart_context, via: [:get, :post, :patch, :delete]
+  match 'cart' => 'carts#show', :as => 'current_cart', via: [:get, :post]
 
   resources :datagroups do
-    resources :categories, only: [:index, :create, :new]
+    resources :categories, only: %i[index create new]
     member do
       get :upload_categories, :datacolumns
       post :update_categories, :import_categories
     end
   end
 
-  resources :categories, only: [:show, :destroy] do
+  resources :categories, only: %i[show destroy] do
     member do
       get :upload_sheetcells
       post :update_sheetcells
@@ -96,18 +101,16 @@ Befchina::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Sample resource route with options:
+  # Example resource route with options:
   #   resources :products do
   #     member do
   #       get 'short'
@@ -119,34 +122,31 @@ Befchina::Application.routes.draw do
   #     end
   #   end
 
-  # Sample resource route with sub-resources:
+  # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
   #   end
 
-  # Sample resource route with more complex sub-resources
+  # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
   #     resources :sales do
-  #       get 'recent', :on => :collection
+  #       get 'recent', on: :collection
   #     end
   #   end
 
-  # Sample resource route within a namespace:
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
