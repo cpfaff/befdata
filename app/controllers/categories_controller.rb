68f4a0ec62809c9bf95626e42f4d25b07c1bdd54
cpfaff <ctpfaff@arcor.de>
2019-11-27 19:40:59 +0100

    fix params

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -113,10 +113,10 @@ class CategoriesController < ApplicationController
   end
 
   def load_datagroup
-    @datagroup = Datagroup.find_by_id(params.require(:datagroup_id).permit(:datagroup_id).to_i)
+    @datagroup = Datagroup.find_by_id(params.require(:datagroup_id).to_i)
   end
 
   def load_category
-    @category = Category.find_by_id(params.require(:id).permit(:id).to_i)
+    @category = Category.find_by_id(params.require(:id).to_i)
   end
 end
