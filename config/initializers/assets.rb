5c4dd8ec3615e78d345c4dd439e069561c15722a
cpfaff <ctpfaff@arcor.de>
2019-12-01 20:42:20 +0100

    Removes comments

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w[site.js site.css ZeroClipboard.min.js jquery-ui/ui-bg_flat_75_ffffff_40x100.png]
-# Rails.application.config.assets.precompile += %w[/.*\.js/,/.*\.css/]
 Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
 Rails.application.config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
