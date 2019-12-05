8d6fc3c14523a67af211356113fee396e1b8e0b1
cpfaff <ctpfaff@arcor.de>
2019-12-05 20:59:36 +0100

    Store old and new secret in configuration

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,4 +10,5 @@ Befchina::Application.config.secret_token = CONFIG[:application_secret_token]
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Befchina::Application.config.secret_key_base = 'e55e4bd4183bb206e89e3a2bde9e0518fb6da7befc70ed88c718bbf24629ff9c486b888ed33a2152a592d0fd9f63739ba297acde665af1e756485592689b4f35'
+Befchina::Application.config.secret_key_base = CONFIG[:application_secret_key_base]
+
