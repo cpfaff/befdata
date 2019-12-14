4283762b1d48b9048e996594dda3ce4c97bbeb86
cpfaff <ctpfaff@arcor.de>
2019-12-14 21:58:46 +0100

    Clean up tests
    * Moves functional to controller test folder
    * Removes performance test related files

--- test/performance/browsing_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-# require 'performance_test_helper'
-
-# # Profiling results for each test method are written to tmp/performance.
-# class BrowsingTest < ActionDispatch::PerformanceTest
-# def test_homepage
-# get '/'
-# end
-# end
