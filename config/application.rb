a799f79e3b8bcc13a3f89e99a71249d8f62729ec
cpfaff <ctpfaff@arcor.de>
2019-12-15 11:21:52 +0100

    According to rake rails update not needed

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require File.expand_path('boot', __dir__)
+# require File.expand_path('boot', __dir__)
 
 require 'rails/all'
-require 'csv'
+# require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
