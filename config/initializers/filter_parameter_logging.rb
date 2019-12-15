63c914679ab7c8881e855eb05f66523cf0aba145
cpfaff <ctpfaff@arcor.de>
2019-12-15 12:04:37 +0100

    Style changes to symbol hash

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,7 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :user_credentials]
+Rails.application.config.filter_parameters += %i[password user_credentials]
 
 # Configure sensitive parameters which will be filtered from the log file.
 # config.filter_parameters += %i[password ]
