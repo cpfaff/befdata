f08b09c6ccf0cd5e8991a499c99d36d306180aaa
cpfaff <ctpfaff@arcor.de>
2020-03-03 15:40:15 +0100

    Removes unused helper method

--- app/helpers/datagroups_helper.rb
+++ app/helpers/datagroups_helper.rb
@@ -1,21 +1,4 @@
 # frozen_string_literal: true
 
 module DatagroupsHelper
-  def sortable(column, title = nil)
-    title ||= column.titleize
-    css_class = column == params[:sort] ? params[:direction] : nil
-    direction = column == params[:sort] && params[:direction] == 'asc' ? 'desc' : 'asc'
-    link_to title, { sort: column, direction: direction, search: params[:search] }, class: css_class
-  end
-
-  def dropdown_list_to_sort_datagroups
-    options_for_select({
-                         'Title' => datagroups_path(params.permit(sort: 'title', direction: 'asc')),
-                         'Recently Added' => datagroups_path(params.permit(sort: 'id', direction: 'desc')),
-                         'Most used' => datagroups_path(params.permit(sort: 'datacolumns_count', direction: 'desc')),
-                         'Least Used' => datagroups_path(params.permit(sort: 'datacolumns_count', direction: 'asc')),
-                         'Most categories' => datagroups_path(params.permit(sort: 'categories_count', direction: 'desc')),
-                         'Least categories' => datagroups_path(params.permit(sort: 'categories_count', direction: 'asc'))
-                       }, selected: datagroups_path(params.permit(:sort, :direction)))
-  end
 end
